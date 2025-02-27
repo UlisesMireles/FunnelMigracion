49ff09c52d93b510da1aaa951a48fcdc077f9035
txiomaraamador <xiomaralizethamadoraguilera@gmail.com>
2025-02-27 15:57:27 -0600

    Front lista de contactos

--- funnel.client/src/app/app.module.ts
+++ funnel.client/src/app/app.module.ts
@@ -11,6 +11,29 @@ import { CambiarContrasenaComponent } from './components/cambiar-contrasena/camb
 import { OlvidasteContrasenaComponent } from './components/olvidaste-contrasena/olvidaste-contrasena.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ProspectosComponent } from './components/catalogos/prospectos/prospectos.component';
+import { ContactosComponent } from './components/catalogos/contactos/contactos.component';
+
+import { MessageService } from 'primeng/api';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button'; 
+import { FormsModule } from '@angular/forms'; 
+import { DropdownModule } from 'primeng/dropdown';
+import { ToastModule } from 'primeng/toast';
+import { InputTextModule } from 'primeng/inputtext';
+import { TagModule } from 'primeng/tag';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { InputIcon } from 'primeng/inputicon';
+import { IconField } from 'primeng/iconfield';
+import { Tag } from 'primeng/tag';
+import { DialogModule } from 'primeng/dialog';
+import { SelectModule } from 'primeng/select';
+import { DatePickerModule } from 'primeng/datepicker';
+import { CheckboxModule } from 'primeng/checkbox';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { TextareaModule } from 'primeng/textarea';
+import { PasswordModule } from 'primeng/password';
+
 
 @NgModule({
   declarations: [
@@ -21,13 +44,35 @@ import { ProspectosComponent } from './components/catalogos/prospectos/prospecto
     CambiarContrasenaComponent,
     OlvidasteContrasenaComponent,
     FooterComponent,
-    ProspectosComponent
+    ProspectosComponent,
+    ContactosComponent,
   ],
   imports: [
     BrowserModule, HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TableModule,
+    ButtonModule,
+    FormsModule,
+    DropdownModule,
+    ToastModule,
+    InputTextModule,
+    TagModule,
+    MultiSelectModule,
+    SelectButtonModule,
+    InputIcon,
+    IconField,
+    Tag,
+    DialogModule,
+    SelectModule,
+    DatePickerModule,
+    CheckboxModule,
+    InputNumberModule,
+    TextareaModule,
+    PasswordModule,
   ],
-  providers: [],
+  providers: [
+    MessageService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

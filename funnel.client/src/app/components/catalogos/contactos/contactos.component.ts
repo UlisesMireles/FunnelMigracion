5cfccc29450c064e667afdd3a09a4b99b3dcf51e
eve264 <gtzhdz79@gmail.com>
2025-03-13 09:49:17 -0600

    Tabla de prueba para filtrado

--- funnel.client/src/app/components/catalogos/contactos/contactos.component.ts
+++ funnel.client/src/app/components/catalogos/contactos/contactos.component.ts
@@ -6,6 +6,9 @@ import { Contacto } from '../../../interfaces/contactos';
 import { ContactosService } from '../../../services/contactos.service';
 import { baseOut } from '../../../interfaces/utils/utils/baseOut';
 import { LoginService } from '../../../services/login.service';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { ColumnasDisponiblesComponent } from '../../shared/columnas-disponibles/columnas-disponibles.component';
+import { sumBy, map as mapping, omit, sortBy, groupBy, keys as getKeys } from "lodash-es";
 
 @Component({
   selector: 'app-contactos',
@@ -13,14 +16,8 @@ import { LoginService } from '../../../services/login.service';
   templateUrl: './contactos.component.html',
   styleUrl: './contactos.component.css'
 })
-export class ContactosComponent {
-  constructor(private contactosService: ContactosService, private messageService: MessageService, private cdr: ChangeDetectorRef,
-    private readonly loginService: LoginService
-  ) { }
+export class ContactosComponent implements OnInit {
 
-  ngOnInit(): void {
-    this.getContactos();
-  }
   @ViewChild('dt') dt!: Table;
 
   contactos: Contacto[] = [];
@@ -32,33 +29,63 @@ export class ContactosComponent {
   first: number = 0;
   rows: number = 10;
 
-  filtroContacto='';
-  filtroNombre='';
-  filtroTelefono='';
-  filtroCorreo='';
-  filtroProspecto='';
+  filtroNombre = '';
+  filtroTelefono = '';
+  filtroCorreo = '';
+  filtroProspecto = '';
 
   insertar: boolean = false;
   modalVisible: boolean = false;
 
-
   EstatusDropdown = [
-    { label: 'Todo', value: null }, 
+    { label: 'Todo', value: null },
     { label: 'Activo', value: 'Activo' },
     { label: 'Inactivo', value: 'Inactivo' },
   ];
+
   rowsOptions = [
     { label: '10', value: 10 },
     { label: '20', value: 20 },
     { label: '50', value: 50 }
   ];
 
+  lsColumnasAMostrar: any[] = [
+    { key: 'nombreCompleto', isCheck: false, valor: 'Nombre Completo', isIgnore: true, isTotal: false, groupColumn: false, tipoFormato: 'text' },
+    { key: 'telefono', isCheck: false, valor: 'Teléfono', isIgnore: true, isTotal: false, groupColumn: false, tipoFormato: 'text' },
+    { key: 'correoElectronico', isCheck: false, valor: 'Correo Electrónico', isIgnore: true, isTotal: false, groupColumn: false, tipoFormato: 'text' },
+    { key: 'prospecto', isCheck: false, valor: 'Prospecto', isIgnore: true, isTotal: false, groupColumn: false, tipoFormato: 'text' },
+    { key: 'desEstatus', isCheck: false, valor: 'Estatus', isIgnore: true, isTotal: false, groupColumn: false, tipoFormato: 'text' },
+  ];
+
+  lsTodasColumnas: any[] = [
+    { key: 'nombreCompleto', isCheck: false, valor: 'Nombre Completo', isIgnore: true, isTotal: false, groupColumn: false, tipoFormato: 'text' },
+    { key: 'telefono', isCheck: false, valor: 'Teléfono', isIgnore: true, isTotal: false, groupColumn: false, tipoFormato: 'text' },
+    { key: 'correoElectronico', isCheck: false, valor: 'Correo Electrónico', isIgnore: true, isTotal: false, groupColumn: false, tipoFormato: 'text' },
+    { key: 'prospecto', isCheck: false, valor: 'Prospecto', isIgnore: true, isTotal: false, groupColumn: false, tipoFormato: 'text' },
+    { key: 'desEstatus', isCheck: false, valor: 'Estatus', isIgnore: true, isTotal: false, groupColumn: false, tipoFormato: 'text' },
+  ];
+
+  columnsAMostrarResp: string = JSON.stringify(this.lsColumnasAMostrar);
+  columnsTodasResp: string = JSON.stringify(this.lsTodasColumnas);
+
+  constructor(
+    private contactosService: ContactosService,
+    private messageService: MessageService,
+    private cdr: ChangeDetectorRef,
+    private readonly loginService: LoginService,
+    public dialog: MatDialog
+  ) { }
+
+  ngOnInit(): void {
+    this.getContactos();
+  }
+
   getContactos() {
     this.contactosService.getContactos(this.loginService.obtenerIdEmpresa()).subscribe({
       next: (result: Contacto[]) => {
         this.contactosOriginal = result;
         this.selectedEstatus = 'Activo';
-        this.cdr.detectChanges(); 
+        this.cdr.detectChanges();
         this.loading = false;
         this.FiltrarPorEstatus();
       },
@@ -72,6 +99,7 @@ export class ContactosComponent {
       },
     });
   }
+
   inserta() {
     this.contactoSeleccionado = {
       idContactoProspecto: 0,
@@ -89,13 +117,13 @@ export class ContactosComponent {
     this.insertar = true;
     this.modalVisible = true;
   }
-  
+
   actualiza(licencia: Contacto) {
     this.contactoSeleccionado = licencia;
     this.insertar = false;
     this.modalVisible = true;
   }
-  
+
   pageChange(event: LazyLoadEvent) {
     if (event.first !== undefined) {
       this.first = event.first;
@@ -104,20 +132,24 @@ export class ContactosComponent {
       this.rows = event.rows;
     }
   }
+
   onInput(event: Event): void {
-    const input = event.target as HTMLInputElement; // Casting de tipo
+    const input = event.target as HTMLInputElement;
     if (this.dt) {
       this.dt.filterGlobal(input.value, 'contains');
     }
   }
+
   prev() {
     this.first = this.first - this.rows;
   }
+
   reset() {
     this.first = 0;
     this.getContactos();
     this.dt.reset();
   }
+
   next() {
     this.first = this.first + this.rows;
   }
@@ -131,7 +163,7 @@ export class ContactosComponent {
       ? dt.filteredValue
       : this.contactos;
     if (campo === 'nombreCompleto') {
-      return registrosVisibles.length; 
+      return registrosVisibles.length;
     }
     return registrosVisibles.reduce(
       (acc: number, empresa: Contacto) =>
@@ -139,12 +171,13 @@ export class ContactosComponent {
       0
     );
   }
+
   isLastPage(): boolean {
     return this.contactos
       ? this.first + this.rows >= this.contactos.length
       : true;
   }
-  
+
   onModalClose() {
     this.modalVisible = false;
   }
@@ -174,4 +207,90 @@ export class ContactosComponent {
       this.dt.first = 0;
     }
   }
+
+  //#region Funciones para el filtrado de columnas y exportación a Excel
+
+  clear(table: Table) {
+    table.clear();
+    this.getContactos();
+    this.lsColumnasAMostrar = JSON.parse(this.columnsAMostrarResp);
+    this.lsTodasColumnas = JSON.parse(this.columnsTodasResp);
+  }
+
+  agregarColumna(event: any) {
+    const targetAttr = event.target.getBoundingClientRect();
+    const dialogConfig = new MatDialogConfig();
+
+    dialogConfig.autoFocus = false;
+    dialogConfig.backdropClass = 'popUpBackDropClass';
+    dialogConfig.panelClass = 'popUpPanelAddColumnClass';
+    dialogConfig.width = '350px';
+
+    dialogConfig.data = {
+      todosColumnas: this.lsTodasColumnas
+    };
+
+    dialogConfig.position = {
+      top: targetAttr.y + targetAttr.height + 10 + "px",
+      left: targetAttr.x - targetAttr.width - 240 + "px"
+    };
+    const dialogRef = this.dialog.open(ColumnasDisponiblesComponent, dialogConfig);
+
+    dialogRef.afterClosed().subscribe(r => {
+      if (r) {
+        this.lsColumnasAMostrar = JSON.parse(this.columnsAMostrarResp);
+        const selectedColumns = r.filter((f: any) => f.isCheck);
+
+        selectedColumns.forEach((element: any) => {
+          this.lsColumnasAMostrar.push(element)
+        });
+      }
+    });
+  }
+
+  exportExcel(table: Table) {
+    let colsIgnorar: any[] = [];
+    
+    // Asegúrate de que dataExport nunca sea undefined
+    let dataExport = (table.filteredValue || table.value || []);
+  
+    this.lsTodasColumnas.forEach(element => {
+      if (!this.lsColumnasAMostrar.find(f => f.key === element.key)) {
+        colsIgnorar.push(element);
+      }
+    });
+  
+    colsIgnorar.forEach(element => {
+      dataExport = mapping(dataExport, i => omit(i, element.key));
+    });
+  
+    import('xlsx').then(xlsx => {
+      const hojadeCalculo: import('xlsx').WorkSheet = xlsx.utils.json_to_sheet(dataExport);
+      const libro: import('xlsx').WorkBook = xlsx.utils.book_new();
+      xlsx.utils.book_append_sheet(libro, hojadeCalculo, "contactos");
+      xlsx.writeFile(libro, "contactos.xlsx");
+    });
+  }
+
+  getTotalCostPrimeNg(table: Table, def: any) {
+    if (def.key == 'nombreCompleto') {
+      return 'Total';
+    }
+
+    if (!def.isTotal) {
+      return
+    }
+
+    if (table.filteredValue !== null && table.filteredValue !== undefined) {
+      return sumBy(this.dt.filteredValue, def.key)
+    }
+
+    return sumBy(this.contactos, def.key)
+  }
+
+  obtenerArregloFiltros(data: any[], columna: string): any[] {
+    const lsGroupBy = groupBy(data, columna);
+    return sortBy(getKeys(lsGroupBy));
+  }
+  //#endregion
 }

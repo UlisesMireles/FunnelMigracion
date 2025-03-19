dadd9ae34dc11bacdb6e7c77dcb404c05b9a29b8
YulissaGonzalez <145691473+YulissaGonzalez@users.noreply.github.com>
2025-03-19 16:17:39 -0600

    Modificaciones en conteo de registros oportunidades perdidas

--- funnel.client/src/app/components/catalogos/oportunidades-perdidas/oportunidades-perdidas.component.ts
+++ funnel.client/src/app/components/catalogos/oportunidades-perdidas/oportunidades-perdidas.component.ts
@@ -44,6 +44,7 @@ export class OportunidadesPerdidasComponent {
   ];
 
   lsTodasColumnas: any[] = [
+    { key: 'idOportunidad', isCheck: true, valor: 'Id', isIgnore: false, isTotal: true, groupColumn: false, tipoFormato: 'text' },
     { key: 'nombre', isCheck: true, valor: 'Nombre', isIgnore: false, isTotal: false, groupColumn: false, tipoFormato: 'text' },
     { key: 'nombreSector', isCheck: false, valor: 'Sector', isIgnore: false, isTotal: false, groupColumn: false, tipoFormato: 'text' },
     { key: 'nombreOportunidad', isCheck: true, valor: 'Oportunidad', isIgnore: false, isTotal: false, groupColumn: false, tipoFormato: 'text' },

27ba0c82440b613b071e899b0bc86cb1dbfb7e4a
YulissaGonzalez <145691473+YulissaGonzalez@users.noreply.github.com>
2025-03-24 17:58:34 -0600

    Creación de comonente para vista clientes top veinte

---
+++ funnel.client/src/app/components/catalogos/top-veinte/top-veinte.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-veinte',
+  standalone: false,
+  templateUrl: './top-veinte.component.html',
+  styleUrl: './top-veinte.component.css'
+})
+export class TopVeinteComponent {
+
+}

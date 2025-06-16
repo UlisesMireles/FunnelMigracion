5aa43b15e9e9e8f96425db83d1e3bedab32cc5fb
txiomaraamador <xiomaralizethamadoraguilera@gmail.com>
2025-06-16 11:15:34 -0600

    component propuesta nuevo inicio

---
+++ funnel.client/src/app/components/inicio/inicio-nuevo/inicio-nuevo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio-nuevo',
+  standalone: false,
+  templateUrl: './inicio-nuevo.component.html',
+  styleUrl: './inicio-nuevo.component.css'
+})
+export class InicioNuevoComponent {
+
+}

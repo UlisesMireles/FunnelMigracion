f33dc16359bb7b6aed4a3fb9b0a3968d3fcf60b0
eve264 <gtzhdz79@gmail.com>
2025-08-27 12:54:51 -0600

    cambio form

--- funnel.client/src/app/components/inicio/registro-contactos/registro-contactos.component.ts
+++ funnel.client/src/app/components/inicio/registro-contactos/registro-contactos.component.ts
@@ -1,16 +1,13 @@
-import { Component, OnInit, ViewChild, ElementRef, HostListener, ChangeDetectorRef } from '@angular/core';
+import { Component, Input, Output, EventEmitter, HostListener, ChangeDetectorRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { ContactosService } from '../../../services/contactos.service';
-import { Contacto } from '../../../interfaces/contactos';
-import { CamposAdicionales } from '../../../interfaces/campos-adicionales';
-import { ModalCamposAdicionalesService } from '../../../services/modalCamposAdicionales.service';
-import { ModalOportunidadesService } from '../../../services/modalOportunidades.service';
+import { environment } from '../../../../environments/environment';
 import { MessageService } from 'primeng/api';
 import { baseOut } from '../../../interfaces/utils/utils/baseOut';
+import { Contacto } from '../../../interfaces/contactos';
+import { ContactosService } from '../../../services/contactos.service';
+import { RequestPContacto } from '../../../interfaces/contactos';
 import { LoginService } from '../../../services/login.service';
-import { Subscription } from 'rxjs';
-import { environment } from '../../../../environments/environment';
-
+import {FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-registro-contactos',
   standalone: false,
@@ -18,23 +15,41 @@ import { environment } from '../../../../environments/environment';
   styleUrl: './registro-contactos.component.css'
 })
 export class RegistroContactosComponent {
-  //Modal Contactos
-  insertarContacto: boolean = true;
-  modalVisibleContactos: boolean = false;
-  contactos: Contacto[] = [];
-  contactoSeleccionado!: Contacto;
-  private modalContactosSubscription!: Subscription;
+  @Input() contacto!: Contacto;
+  @Input() contactos: Contacto[] = [];
+  @Input() title: string = 'Modal';
+  @Input() visible: boolean = true;
+  @Input() insertarContacto: boolean = true;
+  @Input() lecturaProspecto: boolean = false;
+  request!: RequestPContacto;
 
-  //Modal Campos Adicionales
-  camposAdicionales: CamposAdicionales[] = [];
-  camposAdicionalesPorCatalogo: CamposAdicionales[] = [];
-  modalVisibleCamposAdicionales: boolean = false;
-  informacionReferenciaCatalgo: any = {};
+  contactoForm: FormGroup;
+  validaGuadar: boolean = false;
+  prospectos: any[] = [];
+  informacionContactos: Contacto = {
+    idContactoProspecto: 0,
+    bandera: '',
+    nombreCompleto: '',
+    nombre: '',
+    apellidos: '',
+    telefono: '',
+    correoElectronico: '',
+    estatus: 0,
+    desEstatus: '',
+    prospecto: '',
+    idProspecto: 0,
+    idEmpresa: 0
+  };
 
+  idReferencia: number = 0;
+
+  @Output() visibleChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Output() closeModal: EventEmitter<void> = new EventEmitter();
+  @Output() result: EventEmitter<baseOut> = new EventEmitter();
   baseUrl: string = environment.baseURLAssets;
-  constructor(private route: ActivatedRoute, private modalCamposAdicionalesService: ModalCamposAdicionalesService, private modalOportunidadesService: ModalOportunidadesService, private contactosService: ContactosService,
-    private messageService: MessageService, private readonly authService: LoginService,
-  ) { }
+  constructor(private route: ActivatedRoute,private contactosService: ContactosService, private messageService: MessageService, private readonly loginService: LoginService, private fb: FormBuilder, private cdr: ChangeDetectorRef) {
+    this.contactoForm = this.fb.group({});
+  }
 
   ngOnInit(): void {
      this.route.queryParams.subscribe(params => {
@@ -54,156 +69,173 @@ export class RegistroContactosComponent {
       }
       
     });
-     //Suscripcion a servicio de modal de contactos, recibe datos para el despliegue del modal
-     this.modalContactosSubscription = this.modalOportunidadesService.modalContactoState$.subscribe((state) => {
-      this.modalVisibleContactos = state.showModal;
-      this.insertarContacto = state.insertarContacto;
-      this.contactos = state.contactos;
-      this.contactoSeleccionado = state.contactoSeleccionado;
-    });
-     this.agregarContacto();
+    this.inicializarFormulario();
   }
-  agregarContacto() {
-    this.contactoSeleccionado = {
-      idContactoProspecto: 0,
-      nombre: '',
-      apellidos: '',
-      telefono: '',
-      correoElectronico: '',
-      prospecto: '',
-      idEmpresa: 0,
-      idProspecto: 0,
-      estatus: 0,
-      desEstatus: '',
-      nombreCompleto: '',
-      bandera: ''
-    };
-    this.modalOportunidadesService.openModalContacto(true, true, [], this.contactoSeleccionado)
 
+  inicializarFormulario() {
+    let idEmpresa = this.loginService.obtenerIdEmpresa();
+    let valoresIniciales: Record<string, any>;
+    if (this.insertarContacto) {
+      this.idReferencia = 0;
+      this.informacionContactos = {
+        idContactoProspecto: 0,
+        bandera: '',
+        nombreCompleto: '',
+        nombre: this.contacto?.nombre ?? '',
+        apellidos: '',
+        telefono: '',
+        correoElectronico: '',
+        estatus: 0,
+        desEstatus: '',
+        prospecto: '',
+        idProspecto: this.contacto?.idProspecto ?? 0,
+        idEmpresa: 0
+      };
+      this.contactoForm = this.fb.group({
+        idContactoProspecto: [0],
+        nombre: [this.contacto?.nombre ?? '', [
+          Validators.required,
+          Validators.maxLength(50),
+          Validators.pattern('^[a-zA-ZÀ-ÿ\\s]+$')
+        ]
+        ],
+        telefono: ['', [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(20),
+          Validators.pattern('^[0-9+\\-\\s()]+$')
+        ]
+        ],
+        correoElectronico: ['', [Validators.required, Validators.email]],
+        idProspecto: [this.contacto?.idProspecto ?? null, Validators.required],
+        estatus: [true],
+        idEmpresa: [idEmpresa],
+        usuarioCreador: [this.loginService.obtenerIdUsuario()],
+        bandera: ['INSERT']
+      });
+
+
+      
+      this.validaGuadar = false;
+      this.cdr.detectChanges();
+
+    } else {
+      this.idReferencia = this.contacto.idContactoProspecto;
+      this.informacionContactos = this.contacto;
+      this.contactoForm = this.fb.group({
+        idContactoProspecto: [this.contacto?.idContactoProspecto],
+        nombre: [this.contacto?.nombreCompleto, [
+          Validators.required,
+          Validators.maxLength(50),
+          Validators.pattern('^[a-zA-ZÀ-ÿ\\s]+$')
+        ]
+        ],
+        telefono: [this.contacto?.telefono || '', [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(20),
+          Validators.pattern('^[0-9+\\-\\s()]+$')
+        ]
+        ],
+        correoElectronico: [this.contacto?.correoElectronico || '', [Validators.required, Validators.email]],
+        idProspecto: [this.contacto?.idProspecto, Validators.required],
+        estatus: [this.contacto?.estatus === 1],
+        idEmpresa: [idEmpresa],
+        bandera: ['UPDATE'],
+        usuarioCreador: [this.loginService.obtenerIdUsuario()]
+      });
+
+
+      
+      this.validaGuadar = false;
+      this.cdr.detectChanges();
+    }
+    
   }
-  onModalCloseContactos() {
-    this.modalOportunidadesService.closeModalContacto();
+
+  onDialogShow() {
+    this.cargarProspectos();
   }
-  manejarResultadoContactos(result: baseOut) {
-      if (result.result) {
-        this.messageService.add({
-          severity: 'success',
-          summary: 'La operación se realizó con éxito.',
-          detail: result.errorMessage,
-        });
-        this.modalOportunidadesService.closeModalContacto(result);
-      } else {
+
+  close() {
+    this.visible = false;
+    this.visibleChange.emit(this.visible);
+    this.closeModal.emit();
+    if (this.insertarContacto) {
+      this.inicializarFormulario();
+    }
+  }
+
+  guardarContacto() {
+    if (this.contactoForm.invalid) {
+      this.contactoForm.markAllAsTouched();
+      this.mostrarToastError();
+      return;
+    
+    }
+    this.contactoForm.controls['estatus'].setValue(this.contactoForm.value.estatus ? 1 : 0);
+    this.contactoForm.controls['idEmpresa'].setValue(this.loginService.obtenerIdEmpresa());
+    this.contactoForm.controls['bandera'].setValue(this.insertarContacto ? 'INSERT' : 'UPDATE');
+
+    this.informacionContactos = {
+      ...this.informacionContactos,
+      bandera: this.contactoForm.get('bandera')?.value,
+      idContactoProspecto: this.contactoForm.get('idContactoProspecto')?.value,
+      nombre: this.contactoForm.get('nombre')?.value,
+      telefono: this.contactoForm.get('telefono')?.value,
+      correoElectronico: this.contactoForm.get('correoElectronico')?.value,
+      idProspecto: this.contactoForm.get('idProspecto')?.value,
+      estatus: this.contactoForm.get('estatus')?.value,
+      idEmpresa: this.contactoForm.get('idEmpresa')?.value,
+      usuarioCreador: this.contactoForm.get('usuarioCreador')?.value
+    }
+
+    this.contactosService.postContacto(this.informacionContactos).subscribe({
+     next: (result: baseOut) => {
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Éxito',
+        detail: 'El contacto se ha guardado correctamente.'
+      });
+      this.result.emit(result);
+    },
+      error: (error: baseOut) => {
         this.messageService.add({
           severity: 'error',
           summary: 'Se ha producido un error.',
-          detail: result.errorMessage,
+          detail: error.errorMessage,
         });
-      }
-    }
-  
-    manejarResultadoAbrirInputsAdicionales(resut: any) {
-      this.informacionReferenciaCatalgo = resut
-      this.getCamposAdicionales();
-  
-    }
-    manejarResultadoCamposAdicionales(result: baseOut) {
-    if (result.result) {
-      this.messageService.add({
-        severity: 'success',
-        summary: 'La operación se realizó con éxito.',
-        detail: result.errorMessage,
-      });
-      this.modalCamposAdicionalesService.closeModal();
-    } else {
-      this.messageService.add({
-        severity: 'error',
-        summary: 'Se ha producido un error.',
-        detail: result.errorMessage,
-      });
-    }
+      },
+    });
   }
 
-  onModalCloseCamposAdicionales() {
-    this.modalCamposAdicionalesService.closeModal();
-    //Se debe reabrir modal de contacto o prospecto independientemente si guardo datos o no
-    setTimeout(() => {
-      switch (this.informacionReferenciaCatalgo.tipoCatalogo.toLowerCase()) {
-        case "contactos":
-          this.modalOportunidadesService.openModalContacto(true, this.informacionReferenciaCatalgo.insertar, [], this.informacionReferenciaCatalgo.referencia);
-          break;
-        default:
-          this.modalOportunidadesService.openModalProspecto(true, this.informacionReferenciaCatalgo.insertar, [], this.informacionReferenciaCatalgo.referencia)
-          break;
-      }
-    }, 1000);
-  }
-    getCamposAdicionales() {
-
-    const idUsuario = this.authService.obtenerIdUsuario();
-    const idEmpresa = this.authService.obtenerIdEmpresa();
-
-    this.contactosService.getCamposAdicionales(idEmpresa, this.informacionReferenciaCatalgo.tipoCatalogo).subscribe({
-      next: (result: CamposAdicionales[]) => {
-
-        this.camposAdicionales = result.map(campos => ({
-          ...campos,
-          idInput: campos.idInput,
-          nombre: campos.nombre,
-          etiqueta: campos.etiqueta,
-          requerido: campos.requerido,
-          tipoCampo: campos.tipoCampo,
-          rCatalogoInputId: campos.rCatalogoInputId,
-          tipoCatalogoInput: campos.tipoCatalogoInput,
-          orden: campos.orden,
-          idEmpresa: idEmpresa,
-          idUsuario: idUsuario,
-          modificado: false
-        }));
-
-        this.consultarCamposAdicionalesPorCatalogo(idEmpresa, idUsuario);
+  cargarProspectos() {
+    this.contactosService.getProspectos(this.loginService.obtenerIdEmpresa()).subscribe({
+      next: (result: any) => {
+        this.prospectos = result;
+        this.inicializarFormulario();
+        this.cdr.detectChanges();
       },
       error: (error) => {
-        console.error('Error:', error);
         this.messageService.add({
           severity: 'error',
-          summary: 'Error',
-          detail: 'Error al cargar informacion de campos adicionales'
+          summary: 'Se ha producido un error.',
+          detail: error.errorMessage,
         });
-      }
+      },
     });
   }
 
 
-  consultarCamposAdicionalesPorCatalogo(idEmpresa: number, idUsuario: number) {
-
-    this.contactosService.getCamposAdicionalesPorCatalogo(idEmpresa, this.informacionReferenciaCatalgo.tipoCatalogo).subscribe({
-      next: (result: CamposAdicionales[]) => {
-
-        this.camposAdicionalesPorCatalogo = result.map(campos => ({
-          ...campos,
-          idInput: campos.idInput,
-          nombre: campos.nombre,
-          etiqueta: campos.etiqueta,
-          requerido: campos.requerido,
-          tipoCampo: campos.tipoCampo,
-          rCatalogoInputId: campos.rCatalogoInputId,
-          tipoCatalogoInput: campos.tipoCatalogoInput,
-          orden: campos.orden,
-          idEmpresa: idEmpresa,
-          idUsuario: idUsuario,
-          modificado: false
-        }));
-        this.modalCamposAdicionalesService.openModal(true, this.camposAdicionales, this.camposAdicionalesPorCatalogo, this.informacionReferenciaCatalgo.pantalla)
-      },
-      error: (error) => {
-        console.error('Error:', error);
-        this.messageService.add({
-          severity: 'error',
-          summary: 'Error',
-          detail: 'Error al cargar informacion de campos adicionales'
-        });
-      }
+  mostrarToastError() {
+    this.messageService.add({
+      severity: 'error',
+      summary: 'Error',
+      detail: 'Es necesario llenar los campos indicados.',
     });
   }
-
+  esAdministrador(): boolean {
+    const rolAdmin = 1;
+    return this.loginService.obtenerRolUsuario() === rolAdmin;
+  }
 }
